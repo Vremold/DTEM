0ab84b0545d4d79b0badf0e059777cba87265713
Vremold <xremold@gmail.com>
2024-01-06 15:58:33 +0800

    发现协同过滤的实验3有问题了. 给出了相关的说明.

--- RecommendationTasks/ContributionRepo_CF/config.py
+++ RecommendationTasks/ContributionRepo_CF/config.py
@@ -18,9 +18,9 @@ CF_DICT_FILE_PATH = 'RecommendationTasks/ContributionRepo_CF/bin/cf_dict.pkl'
 VALID_TEST_DATA_FILE_PATH   = f'{CONTRIBUTE_FILE_PREFIX}/metric/data/dataset_valid_test.json'
 VALID_RESULT_PATH           = f'RecommendationTasks/ContributionRepo_CF/metric/result/result_valid_test.json'
 
-CONTRIBUTION_FILE_PATH  = 'GHCrawler/cleaned/repo_contributions.txt'
-REPOSITORY_FILE_PATH    = 'GNN/DataPreprocess/full_graph/content/repositories.json'
-CONTRIBUTOR_FILE_PATH   = 'GNN/DataPreprocess/full_graph/content/contributors.json'
+CONTRIBUTION_FILE_PATH      = 'GHCrawler/cleaned/repo_contributions.txt'
+REPOSITORY_FILE_PATH        = 'GNN/DataPreprocess/full_graph/content/repositories.json'
+CONTRIBUTOR_FILE_PATH       = 'GNN/DataPreprocess/full_graph/content/contributors.json'
 
 
 # check files exists
@@ -40,6 +40,11 @@ for it in {
 
 def load_data(filepath: str) -> List[Tuple[int, int, int]]: 
     """
+        see also: 
+            RecommendationTasks/ContributionRepo_CF/metric/metric.py
+        
+        这个函数加载的数据对应 实验4. 
+        
         This function is used to load such these 4 files: 
             - TEST_DATA_FILE_PATH
             - TRAIN_DATA_FILE_PATH
@@ -58,6 +63,15 @@ def load_data(filepath: str) -> List[Tuple[int, int, int]]:
         return json.load(fp)
 
 def load_repo_contributions() -> List[Tuple[int, int, int]]: 
+
+    """
+        see also: 
+            RecommendationTasks/ContributionRepo_CF/metric/metric.py
+        对应其中的 实验3. 
+
+        [DELETING] 这个实验有问题, 这个函数调起的数据和训练 model.bin 的不符. 
+            应该去掉此实验, 此函数也没用了. [/]
+    """
 
     with open(REPOSITORY_FILE_PATH) as fp: 
         repo_idxes = json.load(fp)
